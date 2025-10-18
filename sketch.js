50d5d87e8f9c74c83c612466ee2884dba585b63e
j-ssrf <jasmin.soraruf@mail.polimi.it>
2025-10-18 23:16:10 +0200

    cancella prova scorsa lezione e ricomincia

--- sketch.js
+++ sketch.js
@@ -1,17 +1,9 @@
-function preload() {
-  // put preload code here
+let tabella;
+
+function preload () {
+  tabella = loadTable ("dataset.csv", "csv", "header")
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
-}
-
-function draw() {
-  // put drawing code here
+function setup (){
+  
 }
