a11280bed58ff8ff4a19f7c166831d270f422eb4
j-ssrf <jasmin.soraruf@mail.polimi.it>
2025-10-19 18:49:32 +0200

    canc esempio classe, nuova griglia

--- sketch.js
+++ sketch.js
@@ -5,5 +5,21 @@ function preload () {
 }
 
 function setup (){
-  
+  let margineEsterno = 20
+   let spaziatura = 15;
+  let dimensioneCella = 80;
+
+  // CANVAS E GRIGLIA  COSTELLAZIONI
+  let colonne = floor((windowWidth - margineEsterno * 2) / (dimensioneCella + spaziatura))
+    // floor serve a arrotondare x difetto visto che divido 
+    // larghezza schermo utilizzabile per dimensione celle, 
+    // e se una viene divisa deve andare a capo
+  let righe = ceil(tabella.getRowCount() / colonne);
+    // le righe sono tante quanto righe tabella / colonne arrotondato x eccesso
+  let altezzaTotale = margineEsterno * 2 + righe * dimensioneCella + (righe - 1) * spaziatura;
+    // (se ho 8 righe, ci sono 7 spazi tra loro)
+  createCanvas(windowWidth, altezzaTotale);
+  background(10, 15, 30);
+
+
 }
